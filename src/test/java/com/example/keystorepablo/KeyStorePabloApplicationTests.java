09366275fd592f314faef175c71eb62840640b50
2dam <pabsermat@gmail.com>
2024-01-26 09:51:57 +0100

    Initial commit

---
+++ src/test/java/com/example/keystorepablo/KeyStorePabloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.keystorepablo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeyStorePabloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
